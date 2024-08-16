7462bbb9d50843728553dd2759947f8d814f57bc
Ivan <bendybro04@gmail.com>
2024-08-16 11:22:16 +0300

    version 0.2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from random import  randint
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Визначник переможців')
+button = QPushButton('Згенерувати')
+text = QLabel('Натисни , щоб дізнатися переможця')
+winner = QLabel('?')
+
